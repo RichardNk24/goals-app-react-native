53c0320a2befb37b7df6c9803cbc7263833d2e30
RichardNk24 <richardsobelnkulu@gmail.com>
2022-08-07 22:40:31 +0200

    first commit

---
+++ App.js
@@ -0,0 +1,78 @@
+import { useState } from 'react';
+import { StyleSheet, Text, View, Button, TextInput, FlatList } from 'react-native';
+export default function App() {
+  const [enteredGoalText, setEnteredGoalText] = useState('');
+  const [franceGoals, setFranceGoals] = useState([]);
+
+  function goalInputHandler(enteredText) {
+    setEnteredGoalText(enteredText)
+  };
+
+  function addGoalHandler() {
+    setFranceGoals(currentFranceGoals => [
+      ...franceGoals, enteredGoalText
+    ]);
+  };
+
+  return (
+    <View style={styles.appContainer}>
+      <View style={styles.inputContainer}>
+        <TextInput 
+          style={styles.textInput} 
+          placeholder='Your course goal!' 
+          onChangeText={goalInputHandler} 
+        />
+        <Button title='Add Goal'  onPress={addGoalHandler}/>
+      </View>
+      <View style={styles.goalsContainer} >
+        <FlatList data={franceGoals} 
+        renderItem={(itemData) => {
+          return (
+            <View style={styles.listItem}>
+                <Text style={styles.goalText}>{itemData.item}</Text>
+            </View>
+          )
+        }} 
+        alwaysBounceVertical={false} />
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  appContainer: {
+    flex: 1,
+    paddingTop: 50,
+    paddingHorizontal: 16
+  },
+  inputContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginBottom: 24,
+    borderBottomWidth: 1,
+    borderBottomColor: '#cccccc'
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: '#cccccc',
+    height:'35%' ,
+    width: '70%',
+    marginRight: 2
+  },
+  goalsContainer: {
+    flex: 7
+  },
+  listItem: {
+    margin: 5,
+    padding: 8,
+    borderRadius: 20,
+    alignContent: 'center',
+    backgroundColor: '#1F618D',
+  },
+  goalText: {
+    color: '#FFFFFF',
+    fontSize: 18
+  }
+});
